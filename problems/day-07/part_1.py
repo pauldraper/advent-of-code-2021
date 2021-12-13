8a337fd41f761c1d1d71fd1c13f979a7c3d9efd8
Paul Draper <paulddraper@gmail.com>
2021-12-13 05:05:28 -0700

    Finish day 7

--- problems/day-07/part_1.py
+++ problems/day-07/part_1.py
@@ -6,6 +6,6 @@ crabs = list(map(int, next(sys.stdin).split(",")))
 min_ = float("inf")
 
 for x in range(min(crabs), max(crabs) + 1):
-    min_ = min(min_, sum(abs(c - x) * (abs(c - x) + 1) // 2 for c in crabs))
+    min_ = min(min_, sum(abs(c - x) for c in crabs))
 
 print(min_)
